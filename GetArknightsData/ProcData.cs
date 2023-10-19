1269b9a1e6f5f03e7f48f19cb0ea4c48d0d3f50c
tsctsc6@126.com <tsctsc6@126.com>
2023-10-19 17:03:28 +0800

    更新ProcData.cs

--- GetArknightsData/ProcData.cs
+++ GetArknightsData/ProcData.cs
@@ -9,21 +9,48 @@ namespace GetArknightsData
 {
     public class ProcData
     {
-        static public Dictionary<string, ResourceInfo> CalSynthesisTable()
+        static private Dictionary<string, ResourceInfo>? ResourceDictionary;
+        static private Dictionary<string, int>? depot;
+        static public void CalSynthesisTable()
         {
-            Dictionary<string, ResourceInfo> ResourceDictionary = new();
+            ResourceDictionary = new();
+            List<string>[] ResourceNameByRarity_List = new List<string>[]
+                { new List<string>(), new List<string>(), new List<string>() };
             string json = "";
             try
             {
                 json = File.ReadAllText(@".\Data\材料名单.json");
             }
-            catch(FileNotFoundException) { return ResourceDictionary; }
+            catch(FileNotFoundException) { return; }
             var rc = JsonSerializer.Deserialize<ResourceInfoCollection>(json);
             foreach(var item in rc.resources)
             {
                 ResourceDictionary.Add(item.name, item);
             }
-            return ResourceDictionary;
+            return;
+        }
+        static public Dictionary<string, int> DicOperator(in Dictionary<string, int> dic1, in Dictionary<string, int> dic2,
+            Func<int, int, int> op)
+        {
+            Dictionary<string, int> dic = new(dic1);
+            foreach (var item in dic2)
+            {
+                if (!dic.ContainsKey(item.Key))
+                    dic.Add(item.Key, 0);
+                dic[item.Key] = op(dic[item.Key], item.Value);
+            }
+            return dic;
+        }
+        static public Dictionary<string, int> DicSelfOperator(Dictionary<string, int> dic1, in Dictionary<string, int> dic2,
+            Func<int, int, int> op)
+        {
+            foreach (var item in dic2)
+            {
+                if (!dic1.ContainsKey(item.Key))
+                    dic1.Add(item.Key, 0);
+                dic1[item.Key] = op(dic1[item.Key], item.Value);
+            }
+            return dic1;
         }
         /// <summary>
         /// 获得材料的合成所需
@@ -33,16 +60,16 @@ namespace GetArknightsData
         /// <param name="num">数量</param>
         /// <param name="targetrRrity">要计算到的稀有度</param>
         /// <returns>结果</returns>
-        static public Dictionary<string, int> CalSynthesis(
-            in Dictionary<string, ResourceInfo> ResourceDictionary, string name, int num, int targetrRrity)
+        static public Dictionary<string, int> CalSynthesis(string name, int num, int targetrRrity)
         {
+            if (ResourceDictionary == null) { throw new ArgumentNullException("ResourceDictionary is null"); }
             Dictionary<string, int> dic = new();
-            if (CalSynthesis(ResourceDictionary, name, num, targetrRrity, dic))
+            if (CalSynthesis(name, num, targetrRrity, dic))
                 return dic;
             return new Dictionary<string, int>();
         }
         /// <summary>
-        /// 
+        /// 用于递归
         /// </summary>
         /// <param name="ResourceDictionary">材料合成表</param>
         /// <param name="name">材料名称</param>
@@ -53,33 +80,164 @@ namespace GetArknightsData
         /// false: 计算过程中出现找不到某种材料
         /// true: 计算过程正常
         /// </returns>
-        static private bool CalSynthesis(
-            in Dictionary<string, ResourceInfo> ResourceDictionary, string name, int num, int targetrRrity,
-            Dictionary<string, int> dic)
+        static private bool CalSynthesis(string name, int num, int targetrRrity, Dictionary<string, int> dic)
         {
             ResourceInfo? resourceInfo;
             try { resourceInfo = ResourceDictionary[name]; }
             catch(KeyNotFoundException) { return false; }
+            if (resourceInfo.rarity <= targetrRrity)
+            {
+                if (!dic.ContainsKey(resourceInfo.name))
+                    dic.Add(resourceInfo.name, num);
+                else
+                    dic[resourceInfo.name] = dic[resourceInfo.name] + num;
+                return true;
+            }
             if (resourceInfo.rarity == targetrRrity + 1)
             {
-                if (resourceInfo.synthesisItem.Length == 0) return false;
-                foreach (var si in resourceInfo.synthesisItem)
+                if (resourceInfo.synthesisItems.Length == 0) return true;
+                foreach (var si in resourceInfo.synthesisItems)
                 {
                     if (!dic.ContainsKey(si.name))
                         dic.Add(si.name, si.count * num);
                     else
-                    {
                         dic[si.name] = dic[si.name] + si.count * num;
-                    }
                 }
                 return true;
             }
             if (resourceInfo.rarity > targetrRrity + 1)
-                foreach (var si in resourceInfo.synthesisItem)
-                    if (CalSynthesis(ResourceDictionary, si.name, si.count, targetrRrity, dic))
+            {
+                foreach (var si in resourceInfo.synthesisItems)
+                    if (!CalSynthesis(si.name, si.count * num, targetrRrity, dic))
                         return false;
-            return true;
+                return true;
+            }
+            return false;
         }
-
+        /// <summary>
+        /// 计算一个技能等级专三材料
+        /// </summary>
+        /// <param name="skillResource"></param>
+        /// <param name="targetrRrity"></param>
+        /// <returns></returns>
+        static public Dictionary<string, int> CalSkillResource(SkillResource skillResource, int targetrRrity)
+        {
+            Dictionary<string, int> dic = new();
+            foreach (var r in skillResource.resources)
+            {
+                DicSelfOperator(dic, CalSynthesis(r.name, r.count, targetrRrity), (x, y) => x + y);
+            }
+            return dic;
+        }
+        /// <summary>
+        /// 计算一个技能专三材料
+        /// </summary>
+        /// <param name="skillLevel"></param>
+        /// <param name="targetrRrity"></param>
+        /// <returns></returns>
+        static public Dictionary<string, int> CalSkillLevel(SkillLevel skillLevel, int targetrRrity)
+        {
+            Dictionary<string, int> dic = new();
+            foreach (var r in skillLevel.skillResources)
+            {
+                DicSelfOperator(dic, CalSkillResource(r, targetrRrity), (x, y) => x + y);
+            }
+            return dic;
+        }
+        static public void LoadDepot()
+        {
+            if (ResourceDictionary == null) { throw new ArgumentNullException("ResourceDictionary is null"); }
+            depot = new();
+            string json = "";
+            try
+            {
+                json = File.ReadAllText(@".\Data\depot_res.json");
+            }
+            catch (FileNotFoundException) { return; }
+            var d = JsonSerializer.Deserialize<Depot>(json);
+            foreach (var item in d.items)
+                if (ResourceDictionary.ContainsKey(item.name))
+                    depot.Add(item.name, item.have);
+            return;
+        }
+        /// <summary>
+        /// 计算直接欠缺的材料
+        /// </summary>
+        /// <param name="skillData"></param>
+        /// <returns>直接欠缺的材料</returns>
+        static public Dictionary<string, int> CalLackDirectDepot(Dictionary<string, int> skillData)
+        {
+            if (depot == null) return new();
+            return DicOperator(depot, skillData, (x, y) => x - y);
+        }
+        /// <summary>
+        /// 直接欠缺的材料排序
+        /// </summary>
+        /// <param name="LackDirectDepot"></param>
+        /// <returns></returns>
+        static public List<KeyValuePair<string, int>> CalNeedSynthesis(Dictionary<string, int> LackDirectDepot)
+        {
+            List<KeyValuePair<string, int>> list = new();
+            foreach (var item in LackDirectDepot)
+                if (item.Value < 0)
+                    list.Add(new(item.Key, -item.Value));
+            list.Sort(new KeyValuePair_string_int_Comp(ResourceDictionary));
+            return list;
+        }
+        /// <summary>
+        /// 计算总体欠缺的2级材料
+        /// </summary>
+        /// <param name="skillData"></param>
+        /// <returns>(总计需要合成的材料，总体欠缺的2级材料)</returns>
+        static public (List<KeyValuePair<string, int>>, Dictionary<string, int>)
+            CalLack_Rarity2(Dictionary<string, int> skillData)
+        {
+            if (depot == null) return new();
+            Dictionary<string, int> LackDirectDepot = DicOperator(depot, skillData, (x, y) => x - y);
+            Dictionary<string, int> dicSynthesisPath = new();
+            CalLack_Rarity2(LackDirectDepot, dicSynthesisPath, 4, 2);
+            Dictionary<string, int> dicLack2 = new();
+            foreach (var item in LackDirectDepot)
+                if (item.Value < 0)
+                    dicLack2.Add(item.Key, -item.Value);
+            List<KeyValuePair<string, int>> list = dicSynthesisPath.ToList();
+            list.Sort(new KeyValuePair_string_int_Comp(ResourceDictionary));
+            return (list, dicLack2);
+        }
+        static public void CalLack_Rarity2(Dictionary<string, int> LackDirectDepot,
+            Dictionary<string, int> dicSynthesisPath,
+            int currentRrity, int targetrRrity)
+        {
+            if (currentRrity == targetrRrity) return;
+            var keys = new List<string>(LackDirectDepot.Keys);
+            for(int i = 0; i < keys.Count; i++)
+            {
+                if (LackDirectDepot[keys[i]] < 0 & ResourceDictionary[keys[i]].rarity == currentRrity)
+                {
+                    DicSelfOperator(LackDirectDepot, CalSynthesis(keys[i], -LackDirectDepot[keys[i]], currentRrity - 1),
+                        (x, y) => x - y);
+                    if (!dicSynthesisPath.ContainsKey(keys[i]))
+                        dicSynthesisPath.Add(keys[i], -LackDirectDepot[keys[i]]);
+                    else
+                        dicSynthesisPath[keys[i]] = dicSynthesisPath[keys[i]] - LackDirectDepot[keys[i]];
+                    LackDirectDepot.Remove(keys[i]);
+                }
+            }
+            CalLack_Rarity2(LackDirectDepot, dicSynthesisPath, currentRrity - 1, targetrRrity);
+        }
+    }
+    public class KeyValuePair_string_int_Comp : IComparer<KeyValuePair<string, int>>
+    {
+        private Dictionary<string, ResourceInfo>? ResourceDictionary;
+        public KeyValuePair_string_int_Comp(Dictionary<string, ResourceInfo>? ResourceDictionary)
+        {
+            this.ResourceDictionary = ResourceDictionary;
+        }
+        public int Compare(KeyValuePair<string, int> x, KeyValuePair<string, int> y)
+        {
+            if (!ResourceDictionary.ContainsKey(x.Key) | !ResourceDictionary.ContainsKey(y.Key))
+                throw new ArgumentException("There is no such a key in ResourceDictionary");
+            return ResourceDictionary[x.Key].rarity - ResourceDictionary[y.Key].rarity;
+        }
     }
 }
--- 明日方舟专三材料GUI/MainWindow.xaml.cs
+++ 明日方舟专三材料GUI/MainWindow.xaml.cs
@@ -16,6 +16,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 using GetArknightsData;
 
 namespace 明日方舟专三材料GUI
@@ -25,7 +26,7 @@ namespace 明日方舟专三材料GUI
     /// </summary>
     public partial class MainWindow : Window
     {
-        static internal ResourceInfoCollection? ResourceData = null;
+        static internal Dictionary<string, ResourceInfo>? ResourceDictionary = null;
         static internal List<string>? OperatorList = null;
         static internal OperatorSkill? OperatorSkillData;
         const string depot_res_Path = @".\Data\depot_res.json";
@@ -34,8 +35,11 @@ namespace 明日方舟专三材料GUI
             InitializeComponent();
             try
             {
-                ResourceData = JsonSerializer.Deserialize<ResourceInfoCollection>(File.ReadAllText(GetDataFromWiki.ResourceDataPath));
-                OperatorList = JsonSerializer.Deserialize<OperatorCollection>(File.ReadAllText(GetDataFromWiki.OperatorListPath)).Names.ToList();
+                ResourceDictionary = ProcData.GetResourceDictionary(
+                    JsonSerializer.Deserialize<ResourceInfoCollection>
+                    (File.ReadAllText(GetDataFromWiki.ResourceDataPath)));
+                OperatorList = JsonSerializer.Deserialize<OperatorCollection>
+                    (File.ReadAllText(GetDataFromWiki.OperatorListPath))?.Names.ToList();
                 Label_Time.Content = File.GetLastWriteTime(depot_res_Path).ToString();
                 OperatorNmae_ComboBox.ItemsSource = OperatorList;
             }
@@ -57,10 +61,14 @@ namespace 明日方舟专三材料GUI
 
         private async void Inquare_Button_Click(object sender, RoutedEventArgs e)
         {
-            await GetDataFromWiki.GetSpecializationDataAsync(OperatorNmae_ComboBox.Text).ConfigureAwait(true);
+            OperatorSkillData = await GetDataFromWiki.GetSpecializationDataAsync(OperatorNmae_ComboBox.Text);
+            await Task.Run(() => Proc_Inquare_Async(OperatorSkillData.skills[Skill_ComboBox.SelectedIndex]));
             ShowData();
         }
-
+        private void Proc_Inquare_Async(SkillLevel? data)
+        {
+            
+        }
         private void Skill_ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             ShowData();
@@ -68,7 +76,7 @@ namespace 明日方舟专三材料GUI
 
         private async void Update_Button_Click(object sender, RoutedEventArgs e)
         {
-            ResourceData = await GetDataFromWiki.GetResourceDataAsync();
+            ResourceDictionary = ProcData.GetResourceDictionary(await GetDataFromWiki.GetResourceDataAsync());
             OperatorList = (await GetDataFromWiki.GetOperatorListAsync()).Names.ToList();
             OperatorNmae_ComboBox.ItemsSource = OperatorList;
             MessageBox.Show("更新完成", "提示", MessageBoxButton.OK, MessageBoxImage.Information);
@@ -76,7 +84,36 @@ namespace 明日方舟专三材料GUI
 
         private void ShowData()
         {
+            int SkillIndex = 0;
+            Graph.Plot.Clear();
+            Graph.Render(true);
+            DataGrid_syn.ItemsSource = null;
+            DataGrid_lack.ItemsSource = null;
+            SkillIndex = Skill_ComboBox.SelectedIndex;
+            if (OperatorSkillData?.skills[SkillIndex] == null) return;
+            Dictionary<string, int> res = OperatorSkillData.skills[SkillIndex].Statistics();
+            double[] MatNum = new double[res.Count];
+            int i = 0;
+            foreach (var kv in res)
+            {
+                MatNum[i] = kv.Value;
+                i++;
+            }
+            var bar = Graph.Plot.AddBar(MatNum);
+            bar.ShowValuesAboveBars = true;
+            bar.Font.Bold = true;
+            bar.Font.Size = 18;
 
+            List<KeyValuePair<string, int>>? res_syn;
+            List<KeyValuePair<string, int>>? res_lack;
+            myDepot.Cal(skillData, SkillIndex, out res_syn, out res_lack);
+            List<string> syn_name = new List<string>();
+            List<int> syn_num = new List<int>();
+
+            Graph.Plot.SetAxisLimits(yMin: 0);
+            Graph.Render(true);
+            DataGrid_syn.ItemsSource = res_syn;
+            DataGrid_lack.ItemsSource = res_lack;
         }
 
         private void OpenFile_Button_Click(object sender, RoutedEventArgs e)
