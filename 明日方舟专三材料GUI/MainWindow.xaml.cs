ddc9f6aa3732f12d869c90bba9d6b7ddf10d1ee0
tsctsc6@126.com <tsctsc6@126.com>
2024-05-04 14:25:16 +0800

    重构为mvvm

--- 明日方舟专三材料GUI/MainWindow.xaml.cs
+++ 明日方舟专三材料GUI/MainWindow.xaml.cs
@@ -1,12 +1,4 @@
-﻿using GetArknightsData;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Text.Json;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
 
 namespace 明日方舟专三材料GUI
@@ -16,27 +8,9 @@ namespace 明日方舟专三材料GUI
     /// </summary>
     public partial class MainWindow : Window
     {
-        static internal ProcData? Pc = null;
-        static internal List<string>? OperatorList = null;
-        static internal OperatorSkill? OperatorSkillData;
-        const string depot_res_Path = @".\Data\depot_res.json";
         public MainWindow()
         {
             InitializeComponent();
-            /*
-            try
-            {
-                Pc = new ProcData(JsonSerializer.Deserialize<ResourceInfoCollection>
-                    (File.ReadAllText(GetDataFromWiki.ResourceDataPath)));
-                Pc.LoadDepot(depot_res_Path);
-                OperatorList = JsonSerializer.Deserialize<OperatorCollection>
-                    (File.ReadAllText(GetDataFromWiki.OperatorListPath))?.Names.ToList();
-                Label_Time.Content = File.GetLastWriteTime(depot_res_Path).ToString();
-                OperatorNmae_ComboBox.ItemsSource = OperatorList;
-            }
-            catch (Exception e) { MessageBox.Show(e.Message, "发生错误", MessageBoxButton.OK,
-                MessageBoxImage.Error, MessageBoxResult.OK, MessageBoxOptions.DefaultDesktopOnly);}
-            */
             DataContext = new MainWindowViewModel();
         }
 
@@ -49,37 +23,6 @@ namespace 明日方舟专三材料GUI
         }
 
         /*
-        private void OperatorNmae_ComboBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            OperatorNmae_ComboBox.SelectedIndex = -1;
-            if (!string.IsNullOrEmpty(OperatorNmae_ComboBox.Text))
-            {
-                List<string> searchList = new List<string>();
-                searchList = OperatorList.FindAll(delegate (string s) { return s.Contains(OperatorNmae_ComboBox.Text.Trim()); });
-                OperatorNmae_ComboBox.ItemsSource = searchList;
-            }
-            else OperatorNmae_ComboBox.ItemsSource = OperatorList;
-            OperatorNmae_ComboBox.IsDropDownOpen = true;
-            OperatorSkillData = null;
-            ShowData(null);
-        }
-
-        private async void Inquare_Button_Click(object sender, RoutedEventArgs e)
-        {
-            State_Label.Content = "Running";
-            OperatorSkillData = await GetDataFromWiki.GetSpecializationDataAsync(OperatorNmae_ComboBox.Text);
-            SkillLevel? data;
-            try { data = OperatorSkillData?.skills[Skill_ComboBox.SelectedIndex]; }
-            catch (IndexOutOfRangeException) { ShowData(null); return; }
-            var res = await Task.Run(() => Proc_Inquare_Async(data));
-            ShowData(res);
-        }
-        private (List<KeyValuePair<string, int>>, Dictionary<string, int>)? Proc_Inquare_Async(SkillLevel? data)
-        {
-            if (Pc == null) return null;
-            if (data == null) return null;
-            return Pc.CalLack_Rarity2(Pc.SkillLevelToDic(data));
-        }
         private async void Skill_ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             SkillLevel? data;
@@ -87,46 +30,6 @@ namespace 明日方舟专三材料GUI
             catch (IndexOutOfRangeException) { ShowData(null); return; }
             var res = await Task.Run(() => Proc_Inquare_Async(data));
             ShowData(res);
-        }
-
-        private async void Update_Button_Click(object sender, RoutedEventArgs e)
-        {
-            State_Label.Content = "Running";
-            Pc = new ProcData(await GetDataFromWiki.GetResourceDataAsync());
-            OperatorList = [.. (await GetDataFromWiki.GetOperatorListAsync()).Names];
-            OperatorNmae_ComboBox.ItemsSource = OperatorList;
-            MessageBox.Show("更新完成", "提示", MessageBoxButton.OK, MessageBoxImage.Information, MessageBoxResult.OK, MessageBoxOptions.DefaultDesktopOnly);
-            State_Label.Content = "Done";
-        }
-
-        private void ShowData((List<KeyValuePair<string, int>>, Dictionary<string, int>)? res)
-        {
-            if (res == null)
-            {
-                DataGrid_syn.ItemsSource = null;
-                DataGrid_lack.ItemsSource = null;
-                State_Label.Content = "Done";
-                return;
-            }
-            DataGrid_syn.ItemsSource = res.Value.Item1;
-            DataGrid_lack.ItemsSource = res.Value.Item2;
-            State_Label.Content = "Done";
-        }
-
-        private void OpenFile_Button_Click(object sender, RoutedEventArgs e)
-        {
-            if (!File.Exists(depot_res_Path))
-            {
-                if (!Directory.Exists(@".\Data")) Directory.CreateDirectory(@".\Data");
-                File.WriteAllText(depot_res_Path, "{\"@type\":\"@penguin-statistics/depot\",\"items\":[]}");
-            }
-            Process subp = new Process();
-            subp.StartInfo.FileName = @"C:\WINDOWS\system32\NOTEPAD.EXE";
-            subp.StartInfo.Arguments = depot_res_Path;
-            subp.Start();
-            subp.WaitForExit();
-            Label_Time.Content = File.GetLastWriteTime(depot_res_Path).ToString();
-            Pc?.LoadDepot(depot_res_Path);
         }
         */
     }
