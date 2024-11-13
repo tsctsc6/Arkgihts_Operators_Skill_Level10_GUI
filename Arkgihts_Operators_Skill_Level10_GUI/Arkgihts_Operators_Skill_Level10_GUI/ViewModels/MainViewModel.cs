882c29f3a4cac940715818a42e40a5f69206a877
tsctsc6 <tsctsc6@126.com>
2024-11-13 17:29:48 +0800

    编写获取干员名单的方法

--- Arkgihts_Operators_Skill_Level10_GUI/Arkgihts_Operators_Skill_Level10_GUI/ViewModels/MainViewModel.cs
+++ Arkgihts_Operators_Skill_Level10_GUI/Arkgihts_Operators_Skill_Level10_GUI/ViewModels/MainViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using AngleSharp.Html.Parser;
@@ -12,13 +14,12 @@ public partial class MainViewModel(HttpClient httpClient, HtmlParser htmlParser)
     [ObservableProperty] private string _greeting = "Welcome to Avalonia!";
 
     [RelayCommand]
-    private async Task<string[]> GetOperatorList()
+    private async Task<IEnumerable<string>> GetOperatorListAsync()
     {
         var resp = await httpClient.GetAsync("https://prts.wiki/w/%E5%B9%B2%E5%91%98%E4%B8%80%E8%A7%88");
         resp.EnsureSuccessStatusCode();
         var document = htmlParser.ParseDocument(await resp.Content.ReadAsStringAsync());
-        var operatorData = document.QuerySelectorAll("div#filter-data");
-        var operatorName = operatorData.Select(e => e.Attributes["data-zh"].Value);
-        return [];
+        var operatorData = document.QuerySelector("div#filter-data")?.Children;
+        return operatorData?.Select(e => e.Attributes["data-zh"]?.Value).Where(n => n is not null).Select(n => n!) ?? [];
     }
 }
