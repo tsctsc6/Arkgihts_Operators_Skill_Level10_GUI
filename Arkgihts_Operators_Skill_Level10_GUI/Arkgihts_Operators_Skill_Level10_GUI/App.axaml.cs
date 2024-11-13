333dd2c76f3af7fa8e1935ddfb1f75334575bd91
tsctsc6 <tsctsc6@126.com>
2024-11-13 16:51:20 +0800

    构建DI框架

--- Arkgihts_Operators_Skill_Level10_GUI/Arkgihts_Operators_Skill_Level10_GUI/App.axaml.cs
+++ Arkgihts_Operators_Skill_Level10_GUI/Arkgihts_Operators_Skill_Level10_GUI/App.axaml.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Net.Http;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Data.Core;
@@ -5,11 +7,28 @@ using Avalonia.Data.Core.Plugins;
 using Avalonia.Markup.Xaml;
 using Arkgihts_Operators_Skill_Level10_GUI.ViewModels;
 using Arkgihts_Operators_Skill_Level10_GUI.Views;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Arkgihts_Operators_Skill_Level10_GUI;
 
 public partial class App : Application
 {
+    public new static App Current => (Application.Current as App)!;
+    
+    public IServiceProvider ServiceProvider { get; }
+    
+    public App()
+    {
+        ServiceProvider = ConfigureServices();
+    }
+
+    private IServiceProvider ConfigureServices()
+    {
+        var serviceCollention = new ServiceCollection()
+            .AddSingleton<HttpClient>();
+        return serviceCollention.BuildServiceProvider();
+    }
+    
     public override void Initialize()
     {
         AvaloniaXamlLoader.Load(this);
